2125e0bc0b3094ed8bb8040c71b1722e8b7fdb7a
rexatrom <ajstylejoshua2837@gmail.com>
2025-09-25 20:41:47 +0100

    body-parser installed (Week four)

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const utilities = require("./utilities/")
 const errorRoute = require("./routes/errorRoute")
 const session = require("express-session")
 const pool = require("./database/")
-const bodyParser = require("body-parser")
 
 
 /* ***********************
@@ -43,8 +42,8 @@ app.use(function(req, res, next){
 })
 
 // Body Parsers
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 /* ***********************
